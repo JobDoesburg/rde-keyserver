f35e4f0bc3126c877651a5edd13e635600aae18c
Job Doesburg <job.doesburg@gmail.com>
2022-12-14 17:17:44 +0100

    Fix allowed hosts

--- keyserver/config/settings.py
+++ keyserver/config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG") == "True"
 
-ALLOWED_HOSTS = [os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 BASE_URL = os.environ.get("DJANGO_BASE_URL")
 
